7d0fa20a501cc48859440e32259d9639f4f1dfc2
RaphMt <rafa.mtorres117@outlook.com>
2023-08-26 23:55:11 -0300

    chat finished with user auth

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -4,9 +4,10 @@ const ChatsPage = (props) => {
     return (
         <div className='background'>
             <PrettyChatWindow
-                projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
+                projectId='d0526c92-3388-4a16-8eac-29c24a09a096'
                 username={props.user.username}
                 secret={props.user.secret}
+                style={{height: '100%'}}
             />
         </div>
     )
